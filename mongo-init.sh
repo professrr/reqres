0e25aab421ef20427383558dc9c65e802a3d6625
professrr <a9920377@icloud.com>
2021-12-18 21:46:53 +0300

    mongo-init script upd

---
+++ mongo-init.sh
@@ -0,0 +1,23 @@
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+    var admin = db.getSiblingDB('admin');
+    admin.auth(rootUser, rootPassword);
+    
+    var dbUser = '$MONGO_INITDB_USERNAME';
+    var dbPass = '$MONGO_INITDB_PASSWORD';
+    var dbName = '$MONGO_INITDB_DATABASE';
+
+    db.createUser(
+        {
+            user: dbUser,
+            pwd: dbPass,
+            roles: [
+                {
+                    role: "readWrite",
+                    db: dbName
+                }
+            ]
+        }
+    );
+EOF
